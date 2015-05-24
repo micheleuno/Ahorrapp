2514e3c4a2c266b0ec10b98c3e23a578c158e778
daigosk <diegosk8_@hotmail.com>
2015-05-24 16:55:22 -0300

    v3

--- app/src/main/java/com/ahorrapp/ahorrapp/MapsActivity.java
+++ app/src/main/java/com/ahorrapp/ahorrapp/MapsActivity.java
@@ -3,22 +3,33 @@ package com.ahorrapp.ahorrapp;
 import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+
 
 public class MapsActivity extends FragmentActivity {
 
     private GoogleMap googleMap; ; // Might be null if Google Play services APK is not available.
+
+
 
     /**
      * Initialises the mapview
@@ -64,13 +75,15 @@ public class MapsActivity extends FragmentActivity {
 
 
 
-    /**
-     * Adds a marker to the map
-     */
+    /*
+      Adds a marker to the map
+**/
     private void addMarker(){
 
 
-        /** Make sure that the map has been initialised **/
+        /** Make sure that the map has been initialised*/
+
+
         if(null != googleMap){
             googleMap.addMarker(new MarkerOptions()
                             .position(new LatLng(0,0))
@@ -81,13 +94,15 @@ public class MapsActivity extends FragmentActivity {
     }
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         createMapView();
 
-        final Button marcador = (Button) findViewById(R.id.marcador);
+        final Button marcador = (Button) findViewById(R.id.buscador);
         marcador.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -96,7 +111,32 @@ public class MapsActivity extends FragmentActivity {
             }
         });
 
+        googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+
+            @Override
+            public boolean onMarkerClick(final Marker marker) {
+                Log.e("TESTING", "on Marker click: " + marker.getTitle());
+                if (!marker.isInfoWindowShown())
+                    marker.showInfoWindow();
+                else
+                    marker.hideInfoWindow();
+
+                return true;
+            }
+        });
+
+
     }
+
+
+
+
+
+
+
+
+
+
 
 
 
