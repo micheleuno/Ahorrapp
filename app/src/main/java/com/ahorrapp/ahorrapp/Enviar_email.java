4e2d75283f4dd83af5ae2c54e1b7d04a6bc4bfd3
michele <michele.de.conti@hotmail.com>
2015-08-19 19:07:13 -0300

    asd

--- app/src/main/java/com/ahorrapp/ahorrapp/Enviar_email.java
+++ app/src/main/java/com/ahorrapp/ahorrapp/Enviar_email.java
@@ -1,55 +1,48 @@
 package com.ahorrapp.ahorrapp;
 
-        import java.util.ArrayList;
-        import java.util.HashMap;
-        import java.util.List;
-        import java.util.Properties;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-        import javax.mail.Address;
-        import javax.mail.Authenticator;
-        import javax.mail.Message;
-        import javax.mail.MessagingException;
-        import javax.mail.PasswordAuthentication;
-        import javax.mail.Session;
-        import javax.mail.Transport;
-        import javax.mail.internet.InternetAddress;
-        import javax.mail.internet.MimeMessage;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-        import android.app.Activity;
-        import android.app.ProgressDialog;
-        import android.content.Context;
-        import android.graphics.Typeface;
-        import android.os.AsyncTask;
-        import android.os.Bundle;
-        import android.util.Log;
-        import android.view.View;
-        import android.view.View.OnClickListener;
-        import android.widget.Button;
-        import android.widget.EditText;
-        import android.widget.ListView;
-        import android.widget.TextView;
-        import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
-        import org.apache.http.message.BasicNameValuePair;
-        import org.json.JSONArray;
-        import org.json.JSONException;
-        import org.json.JSONObject;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 /**
  * Created by miche on 19-08-2015.
  */
 public class Enviar_email extends Activity implements OnClickListener{
 
-    private static final String LOGIN_URL = "http://ahorrapp.hol.es/BD/login.php";
-    private static final String TAG_EMAIL = "Email_usuario";
+
     private static final String TAG_SUCCESS = "success";
     JSONArray usuario ;
     JSONParser jsonParser = new JSONParser();
     Session session = null;
     ProgressDialog pdialog = null;
     Context context = null;
-    EditText reciep, sub, msg;
-    String rec, subject, textMessage,contrasena,user;
+    EditText reciep;
+    String rec,contrasena="",user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -107,13 +100,9 @@ public class Enviar_email extends Activity implements OnClickListener{
             try {
                 int success = jsonp.getInt(TAG_SUCCESS);
                 if (success == 1) {
-                    // products found
-                    // Getting Array of Products
                     usuario = jsonp.getJSONArray("usuario");
-                    // looping through All Products
                     for (int j = 0; j < usuario.length(); j++) {
                         JSONObject p = usuario.getJSONObject(j);
-                        // Storing each json item in variable
                          contrasena = p.getString("password");
                          user = p.getString("username");
                     }
@@ -125,25 +114,25 @@ public class Enviar_email extends Activity implements OnClickListener{
         }
 
         protected void onPostExecute(String result){
-            rec = reciep.getText().toString();
-            Properties props = new Properties();
-            props.put("mail.smtp.host", "smtp.gmail.com");
-            props.put("mail.smtp.socketFactory.port", "465");
-            props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-            props.put("mail.smtp.auth", "true");
-            props.put("mail.smtp.port", "465");
-
-            session = Session.getDefaultInstance(props, new Authenticator() {
-                protected PasswordAuthentication getPasswordAuthentication() {
-                    return new PasswordAuthentication("ahorrapp.tds@gmail.com", "micheleuno");
-                }
-            });
-
-            pdialog = ProgressDialog.show(context, "", "Enviando correo...", true);
-
-            RetreiveFeedTask task = new RetreiveFeedTask();
-            task.execute();
-
+            if(!contrasena.equals("")) {
+                rec = reciep.getText().toString();
+                Properties props = new Properties();
+                props.put("mail.smtp.host", "smtp.gmail.com");
+                props.put("mail.smtp.socketFactory.port", "465");
+                props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+                props.put("mail.smtp.auth", "true");
+                props.put("mail.smtp.port", "465");
+                session = Session.getDefaultInstance(props, new Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication("ahorrapp.tds@gmail.com", "micheleuno");
+                    }
+                });
+                pdialog = ProgressDialog.show(context, "", "Enviando correo...", true);
+                RetreiveFeedTask task = new RetreiveFeedTask();
+                task.execute();
+            }else {
+                Alertas.mensaje_error(Enviar_email.this, "Ese correo no esta asociado a ninguna cuenta");
+            }
         }
     }
 }
