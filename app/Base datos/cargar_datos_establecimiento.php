35fc92f18b443f543eb070d78c6a36f016e2440e
michele <michele.de.conti@hotmail.com>
2015-08-19 01:03:00 -0300

    asd

--- app/Base datos/cargar_datos_establecimiento.php
+++ app/Base datos/cargar_datos_establecimiento.php
@@ -2,10 +2,11 @@
 	$response = array();
 	require_once __DIR__ . '/db_config.php';
 	if(!empty($_POST)){
-		$nombre =utf8_encode($_POST['Nombre']);
+		$Latitud =utf8_encode($_POST['Latitud']);
+		$Longitud =utf8_encode($_POST['Longitud']);
 		$conexion = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
 		$query = " SELECT idEstablecimiento,Nombre,Direccion,Descripcion,Contacto FROM Establecimiento
-					Where Nombre='".$nombre."'"; 			
+					Where Latitud = '".$Latitud."' and Longitud = '".$Longitud."'"; 			
 		$result = mysqli_query($conexion, $query); 
 		if (mysqli_num_rows($result) > 0 ) {
 			$response["Establecimiento"] = array();
@@ -26,10 +27,12 @@
 		?>
 		 <h1>Buscar establecimiento</h1> 
 		 <form action="cargar_datos_establecimiento.php" method="post"> 
-		     Nombre establecimiento:<br /> 
-		     <input type="text" name="Nombre" value="" /> 
+		    latitud:<br /> 
+		     <input type="text" name="Latitud" value="" /> 
 		     <br /><br /> 
-		    
+		    latitud:<br /> 
+		     <input type="text" name="Longitud" value="" /> 
+		     <br /><br /> 
 		     <input type="submit" value="Buscar" /> 
 		 </form>
 		 <?php
