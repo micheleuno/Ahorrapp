5309fe45b205cde5dacd51ca9ae30f1516345164
michele <michele.de.conti@hotmail.com>
2015-11-07 11:57:09 -0300

    Ahora se cierra al presionar atras

--- app/src/main/java/com/ahorrapp/ahorrapp/MapsActivity.java
+++ app/src/main/java/com/ahorrapp/ahorrapp/MapsActivity.java
@@ -268,7 +268,10 @@ public class MapsActivity extends FragmentActivity{
     public void onBackPressed() {
         if (doubleBackToExitPressedOnce) {
             super.onBackPressed();
-            return;
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
         }
 
         this.doubleBackToExitPressedOnce = true;
