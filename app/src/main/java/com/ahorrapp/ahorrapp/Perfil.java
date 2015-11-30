2db82fcabe373c15981ea34c7789e0b4bcb6307a
michele <michele.de.conti@hotmail.com>
2015-11-29 22:58:12 -0300

    Cambios en la interfaz

--- app/src/main/java/com/ahorrapp/ahorrapp/Perfil.java
+++ app/src/main/java/com/ahorrapp/ahorrapp/Perfil.java
@@ -1,14 +1,11 @@
 package com.ahorrapp.ahorrapp;
 
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.HashMap;
@@ -32,69 +29,20 @@ public class Perfil extends AppCompatActivity {
         String nombre_pila = user.get(SessionManager.TAG_N_USUARIO);
         String direccion_usuario = user.get(SessionManager.TAG_DIRECCION);
         String email_usuario = user.get(SessionManager.TAG_EMAIL);
-        String id = user.get(SessionManager.TAG_LOCAL);
-        Typeface typeFace=Typeface.createFromAsset(getAssets(),"font/rockwell condensed.ttf");
 
-        TextView nombre =(TextView) findViewById(R.id.txtNombrePersona);
-        nombre.setTypeface(typeFace);
+        TextView nombre = (TextView) findViewById(R.id.txtNombrePersona);
+
         nombre.setText(name);
 
-        TextView direccion =(TextView) findViewById(R.id.txtDireccionPersona);
-        direccion.setTypeface(typeFace);
+        TextView direccion = (TextView) findViewById(R.id.txtDireccionPersona);
         direccion.setText(direccion_usuario);
 
-        TextView email =(TextView) findViewById(R.id.txtemailpersona);
-        email.setTypeface(typeFace);
+        TextView email = (TextView) findViewById(R.id.txtemailpersona);
         email.setText(email_usuario);
 
 
-        TextView username =(TextView) findViewById(R.id.txtEmail);
-        username.setTypeface(typeFace);
+        TextView username = (TextView) findViewById(R.id.txtEmail);
         username.setText(nombre_pila);
-        TextView text1 =(TextView) findViewById(R.id.textbienvenido);
-        text1.setTypeface(typeFace);
-        TextView text2 =(TextView) findViewById(R.id.textdatos);
-        text2.setTypeface(typeFace);
-        TextView text4 =(TextView) findViewById(R.id.textdireccion);
-        text4.setTypeface(typeFace);
-        TextView text6 =(TextView) findViewById(R.id.textnombre);
-        text6.setTypeface(typeFace);
-        TextView text7 =(TextView) findViewById(R.id.textemail);
-        text7.setTypeface(typeFace);
-
-        final Button cerrar = (Button) findViewById(R.id.btncerrar);
-        cerrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-                session.logoutUser();
-            }
-        });
-
-        final Button Solicitar = (Button) findViewById(R.id.btnsolicitar);
-        Solicitar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent nuevoform = new Intent(Perfil.this, Solicitar.class);
-                finish();
-                startActivity(nuevoform);
-            }
-        });
-
-        final Button Negocio = (Button) findViewById(R.id.btnNegocio);
-        if(id.equals("0")) {
-            Negocio.setVisibility(View.INVISIBLE);
-        }
-        Negocio.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(Alertas.Verificar_conexion(Perfil.this)){
-                    Intent nuevoform = new Intent(Perfil.this, Negocio.class);
-                    finish();
-                    startActivity(nuevoform);
-                }
-            }
-        });
     }
     @Override
     public void onBackPressed() {
